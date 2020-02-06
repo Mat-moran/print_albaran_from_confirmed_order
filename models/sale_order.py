d398c96610f7112ae5d5ad499b58094bf9178b5e
mat-moran <amadeo.moran@gmail.com>
2020-02-06 19:42:17 +0100

    si se cumple condicion imprime albarán, si no pedido.

--- models/sale_order.py
+++ models/sale_order.py
@@ -5,9 +5,19 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
 
+
     def get_state_picking(self):
+        """
+        Get picking state of the order. If ordered qtys are equal to delivered
+        qtys done is True
 
-        state = 'done'
+        """
 
-        return state
+        uom_qtys = self.order_line.mapped('product_uom_qty')
+        del_qtys = self.order_line.mapped('qty_delivered')
+
+        done = uom_qtys == del_qtys
+
+
+        return done
 
